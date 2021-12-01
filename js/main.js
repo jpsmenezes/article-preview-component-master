2e99ec8d77cf1d35abaee1c69351fdd7d1ded4e9
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2021-12-01 17:55:13 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('.icon');
 const share = document.querySelector('.share');
+
 button.addEventListener('click', () => {
-       if(share.style.display === 'none'){
-              console.log('teste');
-       }
+       share.classList.toggle('click');      
 });
