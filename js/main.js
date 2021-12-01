e93973bed05755e7f9513f8dcfa6f649d7bf67be
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2021-12-01 18:42:21 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-const button = document.querySelector('.icon');
+const button = document.querySelector('button');
+const buttonActive = document.querySelector('.icon.active');
 const share = document.querySelector('.share');
 
 button.addEventListener('click', () => {
-       share.classList.toggle('click');      
+       share.classList.toggle('click'); 
+       button.classList.toggle('active');
+       if(button.classList.contains('active')){
+              button.style.display = "none";
+              buttonActive.style.display = "flex";
+       }else{
+              button.style.display = "flex";
+              buttonActive.style.display = "none";
+       }          
+});
+buttonActive.addEventListener('click', () => {
+       share.classList.toggle('click'); 
+       button.classList.toggle('active');
+       if(button.classList.contains('active')){
+              button.style.display = "none";
+              buttonActive.style.display = "flex";
+       }else{
+              button.style.display = "flex";
+              buttonActive.style.display = "none";
+       }           
 });
