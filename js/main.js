ecb6bc200da4e196011660c5a9c4edaaa99c9d1b
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2021-12-01 17:08:46 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.icon');
+const share = document.querySelector('.share');
+button.addEventListener('click', () => {
+       if(share.style.display === 'none'){
+              console.log('teste');
+       }
+});
